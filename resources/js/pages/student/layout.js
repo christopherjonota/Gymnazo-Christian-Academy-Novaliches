e384e248bd99010351b5aa003c8cb43af1bea09c
Christopher Jonota <cristopherjonota@gmail.com>
2025-09-25 11:35:21 +0800

    edit layout css

---
+++ resources/js/pages/student/layout.js
@@ -0,0 +1,7 @@
+document.querySelector('.home').addEventListener('click', function() {
+    window.location.href = '/student/home';
+});
+
+document.querySelector('.transaction').addEventListener('click', function() {
+    window.location.href = '/student/transaction';
+});
--- resources/views/layouts/student.blade.php
+++ resources/views/layouts/student.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    @vite('resources/css/student/layout.css')
+    @vite(['resources/css/student/layout.css', 'resources/js/pages/student/layout.js'])
 </head>
 <body>
     <div class="sidebar">
