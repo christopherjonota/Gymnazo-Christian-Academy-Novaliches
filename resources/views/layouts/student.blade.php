520c4ecf6ab6dc5e362a37430a3c899389f0d9b1
Christopher Jonota <cristopherjonota@gmail.com>
2025-09-25 23:33:46 +0800

    add navbar

--- resources/views/layouts/student.blade.php
+++ resources/views/layouts/student.blade.php
@@ -4,15 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    @vite(['resources/css/student/layout.css', 'resources/js/pages/student/layout.js'])
+    @vite(['resources/css/pages/student/layout.css', 'resources/js/pages/student/layout.js'])
 </head>
 <body>
     <div class="sidebar">
         <div class="home">
-
+            <h2>Profile Summary</h2>
         </div>
         <div class="transaction">
-
+             <h2>Class Schedule</h2>
+        </div>
+         <div class="transaction">
+             <h2>Academic Overview</h2>
         </div>
         <button>Dashboard</button>
     </div>
