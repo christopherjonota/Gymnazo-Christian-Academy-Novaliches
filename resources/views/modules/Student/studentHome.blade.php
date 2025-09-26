cd6c6db55c37c7c87b9a2e050054702cd87e98a3
Christopher Jonota <cristopherjonota@gmail.com>
2025-09-27 02:52:02 +0800

    update home structure

--- resources/views/modules/Student/studentHome.blade.php
+++ resources/views/modules/Student/studentHome.blade.php
@@ -3,9 +3,26 @@
     @vite('resources/css/pages/student/home.css')
 @endsection
 @section('content')
-
+<div class="content">
+    <h2>Welcome Back, Christopher!</h2>
     <div class="home-content">
         <h1>Welcome to Student Portal</h1>
     </div>
+    <div class="card-container">
+        <div class="card">
+            <h2>Enrolled Subjects</h2>
+        </div>
+        <div class="card">
+            <h2>Attendance Rate</h2>
+        </div>
+        <div class="card"></div>
+    </div>
+    <div class="schedule">
+        <h1>Today's Schedule</h1>
+    </div>
+    <div class="announcements">
 
+    </div>
+    <div class="access"></div>
+</div>
 @endsection
